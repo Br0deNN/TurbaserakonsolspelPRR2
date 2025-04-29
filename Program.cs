6bb5e4c76a6cc5ad2d8f3d9912d0bcdf6de19b84
7R1XY <theo.broden@gmail.com>
2025-04-29 15:50:40 +0200

    påbörjat

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TheGame
+{
+    abstract class Character
+    {
+        public string Name {get; set;}
+        public int Health {get; set;}
+
+        public int MaxHealth {get; set;}
+
+        public Character(string name, int health)
+        {
+            Name = name;
+            Health = health;
+            MaxHealth = health;
+        }
+
+        public void TakeDamage(int amount){
+            Health -= amount;
+            if (Health < 0 ) Health = 0;
+        }
+
+        public bool isAlive => Health > 0 ;
+
+        public abstract int Attack();
+
+
+    }
+
+    
+
+
+
+
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
