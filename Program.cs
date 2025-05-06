2d00c914646180648408dd934e6a363b9acf1b9e
7R1XY <theo.broden@gmail.com>
2025-05-06 11:08:40 +0200

    bajsmacka

--- Program.cs
+++ Program.cs
@@ -25,10 +25,61 @@ namespace TheGame
 
         public abstract int Attack();
 
+    }
+
+    class Player : Character {
+        public int score {get; private set;}
+
+        public Player(string name, int health) : base(name, health)
+        {
+            score = 0;
+        }
+
+        public void AddScore(int amount)
+        {
+            score += amount;
+        }
+
+        public override int Attack()
+        {
+            return new Random().Next(5,15);
+        }
+    }
+
+    abstract class Enemy : Character
+    {
+        public Enemy(string name, int health) : base(name, health){}
+    }
+
+    class Spider : Enemy 
+    {
+        
+        public Spider() : base("Spider", 20) {}
+
+        public override int Attack()
+        {
+            return new Random().Next(2,6); //Spider gör mellan 2-5 skada
+        }
 
     }
 
+    class Zombie : Enemy 
+    {
+        public Zombie() : base("Zombie", 30){}
+
+        public override int Attack()
+        {
+            return new Random().Next(6,15); //Zombie gör mellan 6-14 skada
+        }
+    }
     
+
+    class Game //Huvudklass för spelet
+    {
+        private Player player; //Spelaren
+        private List<Enemy> enemies; //Lista med fiender
+        private Random random = new Random(); //Genererar slumpade tal
+    }
 
 
 
--- obj/Debug/net8.0/TurbaserakonsolspelPRR2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TurbaserakonsolspelPRR2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TurbaserakonsolspelPRR2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ec5e88008e4d776fa2cd568a851a7d965f679a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb5e4c76a6cc5ad2d8f3d9912d0bcdf6de19b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("TurbaserakonsolspelPRR2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TurbaserakonsolspelPRR2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
